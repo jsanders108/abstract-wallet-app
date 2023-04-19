95b5b2a478085ccfe67ca15c6b70a09063ae66f3
jsanders108 <74698177+jsanders108@users.noreply.github.com>
2023-04-19 18:44:53 -0400

    Update NewWalletGenerator.jsx

--- src/components/NewWalletGenerator.jsx
+++ src/components/NewWalletGenerator.jsx
@@ -6,28 +6,28 @@ import {FACTORY_CONTRACT_ADDRESS} from '../FactoryContractAddress'
 
 function NewWalletGenerator(props) {
 
-    // This state (newWalletAddress) is where the newly generated wallet account will be stored. 
+    // This state (newWalletAddress) is where the newly generated abstract wallet account will be stored. 
     const [newWalletAddress, setNewWalletAddress] = useState("")
   
-    // This function (createSimpleAccount) calls the "createAccount" function in the SimpleAccountFactory Solidity
+    // This function (createSimpleAccount) calls the "createAccount" function in the SimpleAccountFactory.sol
     // contract to create a new abstract wallet account (SimpleAccount.sol). 
     const createSimpleAccount = async () => { 
         
         // Salt is required to generate a new SimpleAccount; this variable (randomSalt) generates a random number for salt. 
         const randomSalt = Math.floor(Math.random() * 1000000000000)
 
-        // Ethers.js is used to connect to the Solidity SimpleAccountFactory contract. 
+        // Ethers.js is used to connect to the SimpleAccountFactory.sol contract. 
         const provider = new ethers.providers.Web3Provider(window.ethereum)
         const signer = provider.getSigner()
         const contract = new ethers.Contract(FACTORY_CONTRACT_ADDRESS, FACTORY_CONTRACT_ABI, signer)
 
-        // The new simple wallet account is created via the Factory contract and a transaction receipt is generated.
-        // The owner address of the new simple wallet is passed in through "props.account" (which is the MetaMask account)
+        // The new abstract wallet account is created via the Factory contract and a transaction receipt is generated.
+        // The owner address of the new wallet is passed in through "props.account" (which is the MetaMask account).
         const txResponse = await contract.createAccount(props.account, randomSalt)
         const txReceipt = await txResponse.wait()
         console.log(txReceipt)
 
-        // The new wallet account's address is recorded in the state variable "newWalletAddress"
+        // The new abstract wallet account's address is recorded in the state variable "newWalletAddress".
         setNewWalletAddress(txReceipt.events[0].address)
     }
     
@@ -40,7 +40,7 @@ function NewWalletGenerator(props) {
                 {/* The "createSimpleAccount" function is called when the user clicks on the "GENERATE" button. */}
                 <button className="blue-btn" onClick={createSimpleAccount}>GENERATE</button>
 
-                {/* The new wallet account address is painted to the user-interface. */}
+                {/* The new abstract wallet account address is painted to the user-interface. */}
                 <p className="new-wallet--address">{newWalletAddress && `Address = ${newWalletAddress}`}</p>
             </div>
         </div>
