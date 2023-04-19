8a56cdc89b83cfa26a9848307abb53eaf6c7e5bb
jsanders108 <74698177+jsanders108@users.noreply.github.com>
2023-04-19 19:08:33 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header(){
     //This component renders the header at the top of the page. 
     return (
         <div className="header--container">
-            <h1 className="header--title">Abstract Wallet Generator App</h1>
+            <h1 className="header--title">Smart Contract Wallet Generator App</h1>
         </div>
     )
 }
